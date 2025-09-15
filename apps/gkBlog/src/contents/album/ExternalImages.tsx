cdc0ce2a579d5a15a3539d9b9e3ec829bbed05e2
TGU-HansJack <2310210201@tiangong.edu.cn>
2025-09-16 00:27:28 +0800

    Update ExternalImages.tsx

--- apps/gkBlog/src/contents/album/ExternalImages.tsx
+++ apps/gkBlog/src/contents/album/ExternalImages.tsx
@@ -1,7 +1,7 @@
 const externalImages: { title: string; url: string }[] = [
   {
     title: "看到好看的主题！",
-    url: "/album/images/theme20250916.png",
+    url: "https://cdn.hansjack.com/2025/09/1bd56d6736e87cd0bc25a90742f908de.webp",
   },
 ];
 
