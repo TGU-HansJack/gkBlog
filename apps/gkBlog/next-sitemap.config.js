b7a0bd78eafd296298f4e3b226f6a543c02e1b7c
TGU-HansJack <2310210201@tiangong.edu.cn>
2025-09-17 01:50:16 +0800

    更新

--- apps/gkBlog/next-sitemap.config.js
+++ apps/gkBlog/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.qladgk.com",
+  siteUrl: "https://www.hansjack.top",
   priority: 0.6,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
